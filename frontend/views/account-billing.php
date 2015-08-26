5bc2438a52d0a9655a127ca7f9b08d03dd9c908a
Will Wharton <willmwharton@gmail.com>
2015-08-25 18:44:32 -0700

    fix menu path and add in panels for frontend view

--- frontend/views/account-billing.php
+++ frontend/views/account-billing.php
@@ -14,7 +14,7 @@ echo $this->render('@wartron/yii2account/views/_alert', ['module' => $module]);
 <div class="row">
     <div class="col-md-3">
         <?php
-            echo $this->render('@wartron/yii2account/views/admin/_menu', ['module' => $module]);
+            echo $this->render('@wartron/yii2account/views/settings/_menu', ['module' => $module]);
         ?>
     </div>
     <div class="col-md-9">
--- frontend/views/account-payments.php
+++ frontend/views/account-payments.php
@@ -15,10 +15,30 @@ echo $this->render('@wartron/yii2account/views/_alert', ['module' => $module]);
 <div class="row">
     <div class="col-md-3">
         <?php
-            echo $this->render('@wartron/yii2account/views/admin/_menu', ['module' => $module]);
+            echo $this->render('@wartron/yii2account/views/settings/_menu', ['module' => $module]);
         ?>
     </div>
     <div class="col-md-9">
-        Frontend Payments
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Billing Information
+            </div>
+            <div class="panel-body">
+
+
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Payment History
+            </div>
+            <div class="panel-body">
+
+
+            </div>
+        </div>
+
+
     </div>
 </div>
