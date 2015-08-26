b614b67e6e74a56996f5954104fb4f8a3e561350
Will Wharton <willmwharton@gmail.com>
2015-08-25 18:46:03 -0700

    add in panels to main billing settings view

--- frontend/views/account-billing.php
+++ frontend/views/account-billing.php
@@ -18,6 +18,23 @@ echo $this->render('@wartron/yii2account/views/_alert', ['module' => $module]);
         ?>
     </div>
     <div class="col-md-9">
-        Frontend Payments
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Billing Information
+            </div>
+            <div class="panel-body">
+
+
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Payment History
+            </div>
+            <div class="panel-body">
+
+
+            </div>
+        </div>
     </div>
 </div>
